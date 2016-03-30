3cff723025464c80d1d172b1c09fb806bdf831ad
Andy Pickler <andy.pickler@gmail.com>
2016-03-29 18:47:19 -0600

    Update dependencies and ESLint

--- learnyounode/.eslintrc.js
+++ learnyounode/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    "extends": "eslint:recommended",
-    "rules": {
-      "no-console": 0,
-      "no-undef": 0,
-      "semi": [2, "always"]
-    }
+  "extends": "eslint:recommended",
+  "rules": {
+    "no-console": 0,
+    "no-undef": 0,
+    "semi": [2, "always"]
+  }
 };
